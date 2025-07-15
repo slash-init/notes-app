847201c75e81816876bb9794f93f705549a8e62e
frenzywho <amvermagaurav007@gmail.com>
2025-07-15 22:49:45 +0530

    added instructions about gist directory

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = '/api/notes' //removed localhost thingy so that frontend can work both locally and in production
 
 const getAll = () => {
     const request = axios.get(baseUrl)
